fc7a6fd1a136b9fcfebbc21164cde6eb2eb1cb2e
SaraOrlian <orlian.sara@gmail.com>
2020-12-14 15:49:17 -0500

    added code from Dr. Katz's sample of basic sql requests

--- StockPNLgenerator/Form1.Designer.cs
+++ StockPNLgenerator/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace StockPNLgenerator
+﻿using System;
+
+namespace StockPNLgenerator
 {
     partial class Form1
     {
@@ -28,13 +30,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbSymbol = new System.Windows.Forms.TextBox();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.nudWprice = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWprice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(192, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Minimal Closee Price";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(555, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Symbol";
+            // 
+            // tbSymbol
+            // 
+            this.tbSymbol.Location = new System.Drawing.Point(622, 39);
+            this.tbSymbol.Name = "tbSymbol";
+            this.tbSymbol.Size = new System.Drawing.Size(89, 26);
+            this.tbSymbol.TabIndex = 4;
+            this.tbSymbol.TextChanged += new System.EventHandler(this.tbSymbol_TextChanged);
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(0, 95);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.RowHeadersWidth = 62;
+            this.dgvData.RowTemplate.Height = 28;
+            this.dgvData.Size = new System.Drawing.Size(798, 352);
+            this.dgvData.TabIndex = 5;
+            // 
+            // nudWprice
+            // 
+            this.nudWprice.Location = new System.Drawing.Point(353, 39);
+            this.nudWprice.Name = "nudWprice";
+            this.nudWprice.Size = new System.Drawing.Size(173, 26);
+            this.nudWprice.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nudWprice);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.tbSymbol);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWprice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbSymbol;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.NumericUpDown nudWprice;
     }
 }
 
--- StockPNLgenerator/Form1.cs
+++ StockPNLgenerator/Form1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +18,69 @@ namespace StockPNLgenerator
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Dr Kat'z method to view the table
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlCon = null; 
+            try
+                {                //sqlCon = new SqlConnection("Server=LCW-101-Win10;Database=MCO343;Trusted_Connection=True;");                
+                                 //sqlCon = new SqlConnection("Server=AKLENOVO\\SQLEXPRESS;Database=WallStreet;Trusted_Connection=True;");                
+                                 //sqlCon = new SqlConnection("Server=DESKTOP-17VOE83;Database=FinanceF20;Trusted_Connection=True;");                
+                    String strServer = ConfigurationManager.AppSettings["server"];
+                    String strDatabase = ConfigurationManager.AppSettings["database"];
+                    String strConnect = $"Server={strServer};Database={strDatabase};Trusted_Connection=True;";
+                    sqlCon = new SqlConnection(strConnect);
+                    sqlCon.Open();
+                    double minPrc = Convert.ToDouble(nudWprice.Value);
+                    String symbol = tbSymbol.Text;
+                    SqlCommand sqlCmd = new SqlCommand("spGetPrcForSymbol", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.Add("@Symbol", System.Data.SqlDbType.VarChar).Value = symbol;
+                    sqlCmd.Parameters.Add("@MinPrc", System.Data.SqlDbType.Float).Value = minPrc;
+                    sqlCmd.ExecuteNonQuery();
+                    SqlDataAdapter da = new SqlDataAdapter(sqlCmd);
+                    DataSet dataset = new DataSet();
+                    da.Fill(dataset, "table1");
+                    dgvData.AutoGenerateColumns = true;
+                    dgvData.DataSource = dataset.Tables["table1"];
+                    //This message box below, shows how to graba specific day's data:
+                    MessageBox.Show($"BTW on {dataset.Tables[0].Rows[1].ItemArray[0]} the price of {symbol} was {dataset.Tables[0].Rows[1].ItemArray[1]}");
+                //dgvData.Refresh();            
+            }            
+                catch (Exception ex)
+                {
+                    MessageBox.Show(" " + DateTime.Now.ToLongTimeString() + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally { if (sqlCon != null && sqlCon.State == System.Data.ConnectionState.Open) sqlCon.Close(); }
+            }
+
+        private void tbSymbol_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void nudWprice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nudWprice_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
+
+
+     
